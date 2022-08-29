d61fabb155ef3c2077cc8a0f007e8a4d02dce556
forst09 <dforst09@gmail.com>
2022-08-29 12:51:12 +0300

    js popup

--- dist/js/app.js
+++ dist/js/app.js
@@ -48,11 +48,12 @@ $(document).ready(function () {
         $(this).toggleClass('active');
     });
 
-
+    //ПО СВАЙПУ УБРАТЬ POP-UP
     $(document).on("click", ".filters__mobile-btn", function () {
-        $(".block").removeClass("swiped");
+        $(".block-swipe").addClass("active");
+        $(".block-swipe__background").addClass("active");
         $("html").addClass("scroll-hidden");
-    })
+    });
 
 
     let touchstartY = 0;
@@ -60,17 +61,69 @@ $(document).ready(function () {
 
     function checkDirection() {
         if (touchendY > touchstartY) {
-            $(".block").addClass("swiped");
+            $(".block-swipe").removeClass("active");
+            $(".block-swipe__background").removeClass("active");
             $("html").removeClass("scroll-hidden");
         }
     }
 
-    document.querySelector('.block').addEventListener('touchstart', e => {
+    document.querySelector('.block-swipe__btn').addEventListener('touchstart', e => {
         touchstartY = e.changedTouches[0].screenY;
-    })
+    });
 
-    document.querySelector('.block').addEventListener('touchend', e => {
+    document.querySelector('.block-swipe__btn').addEventListener('touchend', e => {
         touchendY = e.changedTouches[0].screenY;
         checkDirection();
-    })
+    });
+
+    $(document).mouseup(function (e) {
+        let container = $(".block-swipe");
+        if (container.hasClass('active') && container.has(e.target).length === 0) {
+            $(".block-swipe").removeClass("active");
+            $(".block-swipe__background").removeClass("active");
+            $("html").removeClass("scroll-hidden");
+        }
+    });
+
+    //НАПОЛНЕНИЕ ФИЛЬТРОВ В ЗАВИСИМОСТИ ОТ КОЛИЧЕСТВА
+    if ($(".filters").length !== 0) {
+        const linksArr = [...$(".filters__item")];
+        let linksNum;
+        if ($(window).width() >= 1024 && $(window).width() <= 1499) linksNum = 5;
+        if ($(window).width() >= 1500) linksNum = 8;
+        if (linksArr.length > linksNum) {
+            $.map(linksArr, function (item, index) {
+                $(item).remove();
+                let htmlLink;
+                if (index < linksNum) {
+                    htmlLink = `
+                        <a class="filters__item letter-spacing filters__item-js ${index === 0 ? "active" : ""}" href="javascript:void(0)">${$(item).text()}</a>
+                    `;
+                    $(".filters__items").append(htmlLink);
+                } else if (index === linksNum) {
+                    const html = `
+                        <div class="filters__dots">
+                            <svg>
+                                <use xlink:href="img/icons/sprite.svg#dots"></use>
+                            </svg>
+                            <div class="filters__dots-window-wrapper">
+                                <div class="filters__dots-window">
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                    $(".filters__items").append(html);
+                }
+
+                if (index + 1 > linksNum) {
+                    htmlLink = `
+                        <a href="javascript:void(0)"
+                        class="filters__dots-window-item letter-spacing filters__item-js">${$(item).text()}</a>
+                    `;
+                    $(".filters__dots-window").append(htmlLink);
+                }
+
+            });
+        }
+    }
 });
